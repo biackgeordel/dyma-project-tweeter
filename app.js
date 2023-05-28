74ab8a41e4242e70d40f19f238e8d61a694c5df2
odilon biakabakana <biackgeordel@gmail.com>
2023-05-28 12:23:09 +0200

    mise en place di topbar

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const path = require("path");
-process.env.PORT = 4000;
-const port = process.env.port || 3000;
+const morgan = require("morgan");
 
-const user = require("./scripts/user");
+const port = process.env.PORT || 3000;
+
+const index = require("./routes");
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use(morgan("short"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "css")));
 app.use(express.static(path.join(__dirname, "scripts")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/", user);
+app.use(index);
 app.listen(port, () => {
-  console.log(process.env);
-  console.log("server listen port 8080");
+  console.log(`server listen port: ${port}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-router.use("/index", (req, res) => {
+router.get("/", (req, res) => {
   console.log(router);
 
   res.render("index");
